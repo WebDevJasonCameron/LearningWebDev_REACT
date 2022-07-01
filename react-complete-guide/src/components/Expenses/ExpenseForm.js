1f3569fb0815c69dc532fbf3de9f0a72dd792f95
Jason Cameron <dev.jasoncameron@gmail.com>
2022-07-01 07:36:44 -0500

    multiple useStates vrs state depend

--- react-complete-guide/src/components/Expenses/ExpenseForm.js
+++ react-complete-guide/src/components/Expenses/ExpenseForm.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+	const [enterTitle, setEnterTitle] = useState("");
+	const [enterAmount, SetEnterAmount] = useState("");
+	const [enterDate, setEnterDate] = useState("");
+
+	const titleChangeHandler = (e) => {
+		setEnterTitle(e.target.value);
+	};
+
+	const amountChangeHandler = (e) => {
+		SetEnterAmount(e.target.value);
+	};
+
+	const dateChangeHandler = (e) => {
+		setEnterDate(e.target.value);
+	};
+
 	return (
 		<form>
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" />
+					<input type="text" onChange={titleChangeHandler} />
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" />
+					<input
+						type="number"
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2020-01-01" max="2022-12-31" />
+					<input
+						type="date"
+						min="2020-01-01"
+						max="2022-12-31"
+						onChange={dateChangeHandler}
+					/>
 				</div>
 			</div>
 			<div className="new-expense__actions">
