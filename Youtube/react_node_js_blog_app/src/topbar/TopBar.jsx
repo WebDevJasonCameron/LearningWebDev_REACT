dd4a2d07f1554096e832285b15bea1a56c08ca00
Jason Cameron <dev.jasoncameron@gmail.com>
2022-09-18 16:11:06 -0500

    continue topbar looks

--- Youtube/react_node_js_blog_app/src/topbar/TopBar.jsx
+++ Youtube/react_node_js_blog_app/src/topbar/TopBar.jsx
@@ -3,7 +3,27 @@ import './topbar.css'
 export default function topbar() {
   return (
     <div className='top'>
-      topbar
+      <div className="topLeft">
+        <i className="topIcon fa-brands fa-square-facebook"></i>
+        <i className="topIcon fa-brands fa-square-twitter"></i>
+        <i className="topIcon fa-brands fa-square-pinterest"></i>
+        <i className="topIcon fa-brands fa-square-instagram"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img 
+          className='topImage'
+          src="https://images.unsplash.com/photo-1566753323558-f4e0952af115?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2821&q=80" alt="randomImage"/>
+        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
     </div>
   )
 }
