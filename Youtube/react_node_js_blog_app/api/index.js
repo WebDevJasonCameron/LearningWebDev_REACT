04f6766faa17e4306062664507f98f5956eebb44
Jason Cameron <dev.jasoncameron@gmail.com>
2022-10-13 19:59:13 -0500

    time to move to some tuts that explain this stuff some more.  I will be coming back to this afterwards

--- Youtube/react_node_js_blog_app/api/index.js
+++ Youtube/react_node_js_blog_app/api/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express;
 
+app.request("/", (req, res) => {
+	console.log("Hey this is main url");
+});
+
 app.listen("5000", () => {
 	console.log("Backend is running.");
 });
