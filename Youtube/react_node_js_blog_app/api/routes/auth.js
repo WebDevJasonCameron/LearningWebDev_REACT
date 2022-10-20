6ee47d98a8573e4d8a61e6b59480a52b78ca1040
Jason Cameron <dev.jasoncameron@gmail.com>
2022-10-19 19:32:28 -0500

    think I fixed this

--- Youtube/react_node_js_blog_app/api/routes/auth.js
+++ Youtube/react_node_js_blog_app/api/routes/auth.js
@@ -11,8 +11,8 @@ router.post("/register", async (req, res) => {
 		});
 		const user = await newUser.save();
 		res.status(200).json(user);
-	} catch (e) {
-		res.status(500).json(e);
+	} catch (err) {
+		res.status(500).json(err);
 	}
 });
 
