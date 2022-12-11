69908f9988070c75f681f247dd6f00b4e6ec82fd
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-11 16:14:37 -0600

    fix date

--- Udemy/the_complete_guide/react-complete-guide/src/components/ExpenseItem.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
-	const expenseDate = new Date();
+	const expenseDate = new Date(2021, 2, 28);
 	const expenseTitle = "Car Insurance";
 	const expenseAmount = "294.53";
 	return (
 		<div className="expense-item">
-			<div>{expenseDate}</div>
+			<div>{expenseDate.toISOString()}</div>
 			<div className="expense-item__description">
 				<h2>{expenseTitle}</h2>
 			</div>
-			<div className="expense-item__price">$297.53</div>
+			<div className="expense-item__price">{expenseAmount}</div>
 		</div>
 	);
 }
