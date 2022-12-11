338bbc222fd39b87e98430ac202242e8c2f5ba54
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-11 07:29:46 -0600

    vars in xml

--- Udemy/the_complete_guide/react-complete-guide/src/components/ExpenseItem.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
+	const expenseDate = new Date();
+	const expenseTitle = "Car Insurance";
+	const expenseAmount = "294.53";
 	return (
 		<div className="expense-item">
-			<div>March 28th 2001</div>
+			<div>{expenseDate}</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
+				<h2>{expenseTitle}</h2>
 			</div>
 			<div className="expense-item__price">$297.53</div>
 		</div>
