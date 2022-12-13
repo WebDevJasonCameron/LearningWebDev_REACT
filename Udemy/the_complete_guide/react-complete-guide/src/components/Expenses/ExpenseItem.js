c502660e971ef1426e2f2313cf36250765cf578e
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-12 21:28:05 -0600

    added func to btn

--- Udemy/the_complete_guide/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,10 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+	const clickHandler = () => {
+		console.log("clicked!!!!!!");
+	};
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date} />
@@ -10,13 +14,7 @@ const ExpenseItem = (props) => {
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-			<button
-				onClick={() => {
-					console.log("clicked");
-				}}
-			>
-				Change Title
-			</button>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 };
