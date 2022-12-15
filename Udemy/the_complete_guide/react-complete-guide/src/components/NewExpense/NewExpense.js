f329fceacd05e82a35ff13c3966099588b114310
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-14 19:43:41 -0600

    passing data further up the components... need more work in this concept

--- Udemy/the_complete_guide/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -2,13 +2,13 @@ import React from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
 	const saveExpenseDataHandler = (enteredExpanseData) => {
 		const expenseData = {
 			...enteredExpanseData,
 			id: Math.random().toString(),
 		};
-		console.log(expenseData);
+		props.onAddExpense(expenseData);
 	};
 
 	return (
