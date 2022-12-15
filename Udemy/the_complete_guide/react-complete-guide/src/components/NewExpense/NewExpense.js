0c2fd190fe54002c5875d905f7319a4a723469e0
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-14 19:26:01 -0600

    now moving data up from child to parent

--- Udemy/the_complete_guide/react-complete-guide/src/components/NewExpense/NewExpense.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -3,9 +3,17 @@ import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = () => {
+	const saveExpenseDataHandler = (enteredExpanseData) => {
+		const expenseData = {
+			...enteredExpanseData,
+			id: Math.random().toString(),
+		};
+		console.log(expenseData);
+	};
+
 	return (
 		<div className="new-expense">
-			<ExpenseForm />
+			<ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
 		</div>
 	);
 };
