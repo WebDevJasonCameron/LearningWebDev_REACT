8f4144481480584b6210f249c6b1fd5cbbffaf96
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-17 21:39:09 -0600

    create chartBar component

--- Udemy/the_complete_guide/react-complete-guide/src/components/Chart/ChartBar.js
+++ Udemy/the_complete_guide/react-complete-guide/src/components/Chart/ChartBar.js
@@ -1,8 +1,24 @@
 import React from "react";
+import "./ChartBar.css";
 
 const ChartBar = (p) => {
-	return;
-	pass;
+	let barFillHeight = "0%";
+
+	if (p.max > 0) {
+		barFillHeight = Math.round((p.value / p.maxValue) * 100) + "%";
+	}
+
+	return (
+		<div className="chart-bar">
+			<div className="chart-bar__inner">
+				<div
+					className="chart-bar__fill"
+					style={{ height: barFillHeight }}
+				></div>
+			</div>
+			<div className="chart-bar__label">{p.label}</div>
+		</div>
+	);
 };
 
 export default ChartBar;
