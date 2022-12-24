3bb188ea05ad7b3b8a12a41a9895f5fca05defd3
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-23 21:02:36 -0600

    fix 2nd card. be careful about auto pretty stuff. it can cause probs

--- Udemy/the_complete_guide/03_dedicated_practice_project/src/components/Users/UsersList.js
+++ Udemy/the_complete_guide/03_dedicated_practice_project/src/components/Users/UsersList.js
@@ -5,17 +5,18 @@ import classes from "./UsersList.module.css";
 import Card from "../UI/Card";
 
 const UsersList = (p) => {
-	return;
-	<Card className={classes.users}>
-		<ul>
-			{p.users.map((user) => (
-				<li>
-					{user.name}({user.age} years old){" "}
-				</li>
-			))}
-		</ul>
-		;
-	</Card>;
+	return (
+		<Card className={classes.users}>
+			<ul>
+				{p.users.map((user) => (
+					<li>
+						{user.name}({user.age} years old){" "}
+					</li>
+				))}
+			</ul>
+			;
+		</Card>
+	);
 };
 
 export default UsersList;
