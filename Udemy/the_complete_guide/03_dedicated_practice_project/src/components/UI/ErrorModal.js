59386cf3b7bba061f605d7e2286951864d3c191a
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-25 12:33:14 -0600

    now modal shows the actual error

--- Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
+++ Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
@@ -7,17 +7,20 @@ import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (p) => {
 	return (
-		<Card className={classes.modal}>
-			<header className={classes.header}>
-				<h2>{p.title}</h2>
-			</header>
-			<div className={classes.content}>
-				<p>{p.message}</p>
-			</div>
-			<footer className={classes.actions}>
-				<Button>Okay</Button>
-			</footer>
-		</Card>
+		<div>
+			<div className={classes.backdrop} />
+			<Card className={classes.modal}>
+				<header className={classes.header}>
+					<h2>{p.title}</h2>
+				</header>
+				<div className={classes.content}>
+					<p>{p.message}</p>
+				</div>
+				<footer className={classes.actions}>
+					<Button>Okay</Button>
+				</footer>
+			</Card>
+		</div>
 	);
 };
 
