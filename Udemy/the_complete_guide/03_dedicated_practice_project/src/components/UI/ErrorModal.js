593cf48b7650f55646559b5d7289ad84c935eb6f
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-25 12:23:45 -0600

    add modal, start

--- Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
+++ Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
@@ -3,16 +3,18 @@ import React from "react";
 import Card from "./Card";
 import Button from "./Button";
 
+import classes from "./ErrorModal.module.css";
+
 const ErrorModal = (p) => {
 	return (
-		<Card>
-			<header>
+		<Card className={classes.modal}>
+			<header className={classes.header}>
 				<h2>{p.title}</h2>
 			</header>
-			<div>
-				<p></p>
+			<div className={classes.content}>
+				<p>{p.message}</p>
 			</div>
-			<footer>
+			<footer className={classes.actions}>
 				<Button>Okay</Button>
 			</footer>
 		</Card>
