76c77c19e6c3c1f571ed9d269d25d7f5e1bb0cbe
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-26 13:30:47 -0600

    learning about portals.  access to the html, set ids, and then use react dom

--- Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
+++ Udemy/the_complete_guide/03_dedicated_practice_project/src/components/UI/ErrorModal.js
@@ -1,26 +1,39 @@
 import React from "react";
+import { ReactDOM } from "react";
 
 import Card from "./Card";
 import Button from "./Button";
 
 import classes from "./ErrorModal.module.css";
 
+const Backdrop = (p) => {
+	return <div className={classes.backdrop} onClick={p.onConfirm} />;
+};
+
+const ModalOverlay = (p) => {
+	return (
+		<Card className={classes.modal}>
+			<header className={classes.header}>
+				<h2>{p.title}</h2>
+			</header>
+			<div className={classes.content}>
+				<p>{p.message}</p>
+			</div>
+			<footer className={classes.actions}>
+				<Button onClick={p.onConfirm}>Okay</Button>
+			</footer>
+		</Card>
+	);
+};
+
 const ErrorModal = (p) => {
 	return (
-		<div>
-			<div className={classes.backdrop} onClick={p.onConfirm} />
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h2>{p.title}</h2>
-				</header>
-				<div className={classes.content}>
-					<p>{p.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={p.onConfirm}>Okay</Button>
-				</footer>
-			</Card>
-		</div>
+		<React.Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop onConfirm={p.onConfirm} />,
+				document.getElementById("backdrop-root")
+			)}
+		</React.Fragment>
 	);
 };
 
