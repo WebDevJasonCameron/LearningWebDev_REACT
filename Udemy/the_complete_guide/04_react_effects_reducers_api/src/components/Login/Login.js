8f9c8c123f0231c44dbb7d4fde83f7d5e4b33091
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-28 19:53:47 -0600

    cont with lsn on useReducer... useEffect isnt used though

--- Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/Login/Login.js
+++ Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/Login/Login.js
@@ -9,8 +9,8 @@ const emailReducer = (state, action) => {
 };
 
 const Login = (props) => {
-	const [enteredEmail, setEnteredEmail] = useState("");
-	const [emailIsValid, setEmailIsValid] = useState();
+	// const [enteredEmail, setEnteredEmail] = useState("");
+	// const [emailIsValid, setEmailIsValid] = useState();
 	const [enteredPassword, setEnteredPassword] = useState("");
 	const [passwordIsValid, setPasswordIsValid] = useState();
 	const [formIsValid, setFormIsValid] = useState(false);
@@ -35,7 +35,7 @@ const Login = (props) => {
 	// }, [enteredEmail, enteredPassword]);
 
 	const emailChangeHandler = (event) => {
-		setEnteredEmail(event.target.value);
+		dispatchEmail({ type: "USER_INPUT", val: event.target.value });
 
 		setFormIsValid(
 			event.target.value.includes("@") && enteredPassword.trim().length > 6
@@ -45,13 +45,11 @@ const Login = (props) => {
 	const passwordChangeHandler = (event) => {
 		setEnteredPassword(event.target.value);
 
-		setFormIsValid(
-			emailState.value.includes("@") && event.target.value.trim().length > 6
-		);
+		setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
 	};
 
 	const validateEmailHandler = () => {
-		setEmailIsValid(emailState.value.includes("@"));
+		setEmailIsValid(emailState.isValid);
 	};
 
 	const validatePasswordHandler = () => {
@@ -60,7 +58,7 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(enteredEmail, enteredPassword);
+		props.onLogin(emailState.value, enteredPassword);
 	};
 
 	return (
@@ -68,14 +66,14 @@ const Login = (props) => {
 			<form onSubmit={submitHandler}>
 				<div
 					className={`${classes.control} ${
-						emailIsValid === false ? classes.invalid : ""
+						emailState.isValid === false ? classes.invalid : ""
 					}`}
 				>
 					<label htmlFor="email">E-Mail</label>
 					<input
 						type="email"
 						id="email"
-						value={enteredEmail}
+						value={emailState.value}
 						onChange={emailChangeHandler}
 						onBlur={validateEmailHandler}
 					/>
