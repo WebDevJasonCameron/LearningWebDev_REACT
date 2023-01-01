36f2335731985bf8698c80b8582f8b46125c78af
Jason Cameron <dev.jasoncameron@gmail.com>
2022-12-31 18:27:48 -0600

    fin useContext with const var

--- Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/MainHeader/Navigation.js
+++ Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/MainHeader/Navigation.js
@@ -6,31 +6,25 @@ import classes from "./Navigation.module.css";
 const Navigation = (props) => {
 	const ctx = useContext(AuthContext);
 	return (
-		<AuthContext.Consumer>
-			{() => {
-				return (
-					<nav className={classes.nav}>
-						<ul>
-							{ctx.isLoggedIn && (
-								<li>
-									<a href="/">Users</a>
-								</li>
-							)}
-							{props.isLoggedIn && (
-								<li>
-									<a href="/">Admin</a>
-								</li>
-							)}
-							{props.isLoggedIn && (
-								<li>
-									<button onClick={props.onLogout}>Logout</button>
-								</li>
-							)}
-						</ul>
-					</nav>
-				);
-			}}
-		</AuthContext.Consumer>
+		<nav className={classes.nav}>
+			<ul>
+				{ctx.isLoggedIn && (
+					<li>
+						<a href="/">Users</a>
+					</li>
+				)}
+				{ctx.isLoggedIn && (
+					<li>
+						<a href="/">Admin</a>
+					</li>
+				)}
+				{ctx.isLoggedIn && (
+					<li>
+						<button onClick={props.onLogout}>Logout</button>
+					</li>
+				)}
+			</ul>
+		</nav>
 	);
 };
 
