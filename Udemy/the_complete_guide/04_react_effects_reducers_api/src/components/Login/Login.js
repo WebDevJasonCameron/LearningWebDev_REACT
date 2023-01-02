246c6de4898b15268890b9569d0ff508081587f6
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-02 07:14:07 -0600

    study more on props, usecontex, and usestate

--- Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/Login/Login.js
+++ Udemy/the_complete_guide/04_react_effects_reducers_api/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from "react";
+import React, { useState, useEffect, useReducer, useContext } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
+import AuthContext from "../../Store/auth-context";
 
 const emailReducer = (state, action) => {
 	if (action.type === "USER_INPUT") {
@@ -24,7 +25,7 @@ const passwordReducer = (state, action) => {
 	return { value: "", isValid: false };
 };
 
-const Login = (props) => {
+const Login = () => {
 	const [formIsValid, setFormIsValid] = useState(false);
 
 	const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -36,6 +37,8 @@ const Login = (props) => {
 		value: "",
 		isValid: null,
 	});
+
+	const authCtx = useContext(AuthContext);
 
 	const { isValid: emailIsValid } = emailState; // obj destruction ... giving an alias
 	const { isValid: passwordIsValid } = passwordState; // the end product keeps useEffect from cont to run after true
@@ -72,7 +75,7 @@ const Login = (props) => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		props.onLogin(emailState.value, passwordState.value);
+		authCtx.onLogin(emailState.value, passwordState.value);
 	};
 
 	return (
