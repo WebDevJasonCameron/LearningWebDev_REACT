7df1c4bedb79bc1394d781bee8bf26ab765b8bdc
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-04 15:19:35 -0600

    added header

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
@@ -1,10 +1,18 @@
-import { Fragment } from "react";
+import React, { Fragment } from "react";
+
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 
 const Header = (p) => {
 	return (
 		<React.Fragment>
-			<header></header>
-			<div></div>
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<button>Cart</button>
+			</header>
+			<div className={classes["main-image"]}>
+				<img src={mealsImage} alt="A table full of delicious food!" />
+			</div>
 		</React.Fragment>
 	);
 };
