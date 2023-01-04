5de5b493ad2af3b25ee1c0c54de4cc31b1a37bdf
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-04 07:30:05 -0600

    prep proj ;

---
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Header = (p) => {
+	return (
+		<React.Fragment>
+			<header></header>
+			<div></div>
+		</React.Fragment>
+	);
+};
+
+export default Header;
