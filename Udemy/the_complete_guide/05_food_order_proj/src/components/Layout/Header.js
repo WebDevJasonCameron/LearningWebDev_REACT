d5ae035fd955a8c4c0cef62ebddb8b1a3173c0e4
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-04 16:13:29 -0600

    add styled button to header

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCardButton from "./HeaderCartButton";
+
 import classes from "./Header.module.css";
 
 const Header = (p) => {
@@ -8,7 +10,7 @@ const Header = (p) => {
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCardButton />
 			</header>
 			<div className={classes["main-image"]}>
 				<img src={mealsImage} alt="A table full of delicious food!" />
