3779beafe5ded466864be85a58bfbdccbdfdde46
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-04 16:25:10 -0600

    con w/ adding more meals modules

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
-import HeaderCardButton from "./HeaderCartButton";
+import HeaderCartButton from "./HeaderCartButton";
 
 import classes from "./Header.module.css";
 
@@ -10,7 +10,7 @@ const Header = (p) => {
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<HeaderCardButton />
+				<HeaderCartButton />
 			</header>
 			<div className={classes["main-image"]}>
 				<img src={mealsImage} alt="A table full of delicious food!" />
