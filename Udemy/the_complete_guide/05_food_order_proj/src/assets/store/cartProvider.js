ad2730209284e99660a6af7422434595d2d3762c
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-05 13:31:56 -0600

    creating the add item func to the cart

--- Udemy/the_complete_guide/05_food_order_proj/src/assets/store/cartProvider.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/assets/store/cartProvider.js
@@ -1,12 +1,43 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+	if (action.type == "ADD") {
+		const updatedItems = state.items.concat(action.item);
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
+	return defaultCartState;
+};
+
 const CartProvider = (p) => {
-	const addItemToCartHandler = (i) => {};
-	const removeItemFromCartHandler = (i) => {};
+	const [cartState, despatchCartAction] = useReducer(
+		cartReducer,
+		defaultCartState
+	);
+
+	const addItemToCartHandler = (item) => {
+		despatchCartAction({ type: "ADD", item: item });
+	};
+
+	const removeItemFromCartHandler = (id) => {
+		despatchCartAction({ type: "REMOVE", id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmout: 0,
+		items: cartState.items,
+		totalAmout: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
--- Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/HeaderCartButton.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,24 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../assets/store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (p) => {
+	const cartCtx = useContext(CartContext);
+
+	const numberOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
+		return currentNumber + item.amount;
+	}, 0);
+
 	return (
 		<button className={classes.button} onClick={p.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
 			<span>Your Cart</span>
-			<span className={classes.badge}>3</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
