441fa1a1f8825362d7146c298229ea4a3db6d119
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-07 10:21:20 -0600

    adding a useRef. see mealItemForm.js and input.js on how we pass the useRef to the user created compo

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Meals/MealItem/MealItemForm.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,30 @@
+import { useRef } from 'react'
+
 import Input from "../../UI/Input";
 
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (p) => {
+
+	const amountInputRef = useRef();
+
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+
+	}
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<Input
+				ref={amountInputRef}
 				label="Amount"
 				input={{
 					id: "amount",
 					type: "number",
 					min: "1",
-					max: "5",
+					max: "5",blank
 					step: "1",
 					defaultValue: "1",
 				}}
--- Udemy/the_complete_guide/05_food_order_proj/src/components/UI/Input.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/UI/Input.js
@@ -1,12 +1,14 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (p) => {
+const Input = React.forwardRef((p, ref) => {
 	return (
 		<div className={classes.input}>
 			<label htmlFor={p.input.id}>{p.label}</label>
-			<input {...p.input} />
+			<input ref={...p.input} />
 		</div>
 	);
-};
+});
 
 export default Input;
