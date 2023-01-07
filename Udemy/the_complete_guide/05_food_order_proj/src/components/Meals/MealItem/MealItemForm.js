f129572cb24c3f807e82383a07b9a78de14faee5
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-07 10:38:45 -0600

    add useState to mealItemForm.js that handles errors in the input

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Meals/MealItem/MealItemForm.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Meals/MealItem/MealItemForm.js
@@ -1,19 +1,30 @@
-import { useRef } from 'react'
+import { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (p) => {
-
+	const [amountIsValid, setAmountIsValid] = useState(true);
 	const amountInputRef = useRef();
-
 
 	const submitHandler = (e) => {
 		e.preventDefault();
 
+		const enteredAmount = amountInputRef.current.value;
+		const enteredAmountNumber = +enteredAmount;
 
-	}
+		if (
+			enteredAmount.trim().length === 0 ||
+			enteredAmountNumber < 1 ||
+			enteredAmountNumber > 5
+		) {
+			setAmountIsValid(false);
+			return;
+		}
+
+		p.onAddToCart(enteredAmountNumber);
+	};
 
 	return (
 		<form className={classes.form} onSubmit={submitHandler}>
@@ -24,12 +35,13 @@ const MealItemForm = (p) => {
 					id: "amount",
 					type: "number",
 					min: "1",
-					max: "5",blank
+					max: "5",
 					step: "1",
 					defaultValue: "1",
 				}}
 			/>
 			<button>+ Add</button>
+			{!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
 		</form>
 	);
 };
