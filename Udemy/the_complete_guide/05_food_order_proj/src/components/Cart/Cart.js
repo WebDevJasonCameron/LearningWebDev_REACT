ba76f3b26ea0d7d321bd826a363e44c4541457ce
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-07 21:38:42 -0600

    trying to fix the blank prob

--- Udemy/the_complete_guide/05_food_order_proj/src/components/Cart/Cart.js
+++ Udemy/the_complete_guide/05_food_order_proj/src/components/Cart/Cart.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 
 import Modal from "../UI/Modal";
 import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 import classes from "./Cart.module.css";
 
@@ -11,10 +12,21 @@ const Cart = (p) => {
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
 	const hasItems = cartCtx.items.length > 0;
 
+	const cartItemRemoveHandler = (id) => {};
+
+	const cartItemAddHandler = (item) => {};
+
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
 			{cartCtx.items.map((item) => (
-				<li>{item.name}</li>
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
 			))}
 		</ul>
 	);
