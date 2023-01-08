ebcb62739d42aab4d91998289c9a7401c93b43ae
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-08 15:41:02 -0600

    add btn to show par.  look in console

--- Udemy/the_complete_guide/06_react_behind_the_scene/src/App.js
+++ Udemy/the_complete_guide/06_react_behind_the_scene/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import './App.css';
+import Button from "./components/UI/Button/Button";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showPar, setShowPar] = useState(false);
+
+	const toggleParHandler = () => {
+		setShowPar((prevShowPar) => !prevShowPar);
+	};
+
+	return (
+		<div className="app">
+			<h1>Hi there!</h1>
+			<Button onClick={toggleParHandler}>Toggle Paragraph!</Button>
+			{showPar && <p>This is new!</p>}
+		</div>
+	);
 }
 
 export default App;
