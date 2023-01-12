87453588f2c7e54eb3bf7667f8df6507625b7df3
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-11 19:56:39 -0600

    nothing he teaches actually works in this part of the tut

--- Udemy/the_complete_guide/06_react_behind_the_scene/src/App.js
+++ Udemy/the_complete_guide/06_react_behind_the_scene/src/App.js
@@ -7,16 +7,25 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
 	const [showPar, setShowPar] = useState(false);
+	const [allowToggle, setAllowToggle] = useState(false);
 	console.log("APP RUNNING");
 
 	const toggleParHandler = useCallback((e) => {
 		e.preventDefault(); // must add this or you never see the con log
-		setShowPar((prevShowPar) => !prevShowPar);
+		if (allowToggle) {
+			setShowPar((prevShowPar) => !prevShowPar);
+		}
 	}, []);
+
+	const allowToggleHandler = (e) => {
+		e.preventDefault();
+		setAllowToggle(true);
+	};
 
 	return (
 		<div className="app">
 			<h1>Hi there!</h1>
+			<Button onClick={allowToggleHandler}>Allow Toggling</Button>
 			<Button onClick={toggleParHandler}>Toggle Paragraph!</Button>
 			<DemoOutput show={showPar} />
 		</div>
