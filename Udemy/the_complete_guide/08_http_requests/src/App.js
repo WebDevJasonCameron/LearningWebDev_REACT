23401e8b768c33e5085da0ec99bb53bc58504562
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-15 07:50:08 -0600

    back to grabing swapi api data, but with react!

--- Udemy/the_complete_guide/08_http_requests/src/App.js
+++ Udemy/the_complete_guide/08_http_requests/src/App.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import MoviesList from './components/MoviesList';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const [movies, setMovies] = useState([]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	function fetchMoviesHandler() {
+		fetch("https://swapi.dev/api/films/")
+			.then((r) => {
+				return r.json();
+			})
+			.then((d) => {
+				const transformedMovies = d.resuts.map((movieData) => {
+					return {
+						id: movieData.episode_id,
+						title: movieData.title,
+						openingText: movieData.opening_crawl,
+						relaseData: movieData.release_date,
+					};
+				});
+				setMovies(transformedMovies);
+			});
+	}
+
+	return (
+		<React.Fragment>
+			<section>
+				<button>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
