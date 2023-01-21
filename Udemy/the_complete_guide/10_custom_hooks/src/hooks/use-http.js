4a2174aba14f30b494810863eb46869ca9518f6a
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-21 08:25:44 -0600

    refactor app. sends ok.  broke on read

--- Udemy/the_complete_guide/10_custom_hooks/src/hooks/use-http.js
+++ Udemy/the_complete_guide/10_custom_hooks/src/hooks/use-http.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 import(useState);
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const sendRequest = async (taskText) => {
+	const sendRequest = useCallback(async (requestConfig, applyData) => {
 		setIsLoading(true);
 		setError(null);
 		try {
@@ -26,7 +26,7 @@ const useHttp = (requestConfig, applyData) => {
 			setError(err.message || "Something went wrong!");
 		}
 		setIsLoading(false);
-	};
+	}, []);
 
 	return {
 		isLoading,
