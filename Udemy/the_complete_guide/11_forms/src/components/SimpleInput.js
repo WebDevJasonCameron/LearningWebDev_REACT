649bc62e0488950d8ce5ea0cbadef2ff874e0a85
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-22 08:20:52 -0600

    started next lsn with changing simpleinput.js

--- Udemy/the_complete_guide/11_forms/src/components/SimpleInput.js
+++ Udemy/the_complete_guide/11_forms/src/components/SimpleInput.js
@@ -1,15 +1,25 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const [enteredName, setEnteredName] = useState("");
+
+	const nameInputChangeHandler = (e) => {
+		setEnteredName(e.target.value);
+	};
+
+	const formSubmissionHandler = (e) => {};
+
+	return (
+		<form onSubmit={formSubmissionHandler}>
+			<div className="form-control">
+				<label htmlFor="name">Your Name</label>
+				<input type="text" id="name" onChange={nameInputChangeHandler} />
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
