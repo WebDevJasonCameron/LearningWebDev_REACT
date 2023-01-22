a2f7d22137d097796017fd9d75754769761b31c5
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-22 08:56:06 -0600

    work on form sub.  cl still does not work

--- Udemy/the_complete_guide/11_forms/src/components/SimpleInput.js
+++ Udemy/the_complete_guide/11_forms/src/components/SimpleInput.js
@@ -1,19 +1,31 @@
 import { useRef, useState } from "react";
 
 const SimpleInput = (props) => {
+	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState("");
 
 	const nameInputChangeHandler = (e) => {
 		setEnteredName(e.target.value);
 	};
 
-	const formSubmissionHandler = (e) => {};
+	const formSubmissionHandler = (e) => {
+		e.preventDefault();
+
+		console.log(enteredName);
+		const enteredValue = nameInputRef.current.value;
+		console.log(enteredValue);
+	};
 
 	return (
 		<form onSubmit={formSubmissionHandler}>
 			<div className="form-control">
 				<label htmlFor="name">Your Name</label>
-				<input type="text" id="name" onChange={nameInputChangeHandler} />
+				<input
+					ref={nameInputRef}
+					type="text"
+					id="name"
+					onChange={nameInputChangeHandler}
+				/>
 			</div>
 			<div className="form-actions">
 				<button>Submit</button>
