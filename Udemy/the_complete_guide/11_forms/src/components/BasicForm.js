b6e560b7bd6a239d4153e36ea8f66d663d4cc21c
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-26 19:54:01 -0600

    comp second form on w/o vid

--- Udemy/the_complete_guide/11_forms/src/components/BasicForm.js
+++ Udemy/the_complete_guide/11_forms/src/components/BasicForm.js
@@ -1,25 +1,108 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
-  return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
-      </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
-      </div>
-      <div className='form-actions'>
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const {
+		value: enterFirstName,
+		isValid: enteredFirstNameIsValid,
+		hasError: firstNameHasError,
+		valueChangeHandler: firstNameChangeHandler,
+		inputBlurHandler: firstNameBlurHandler,
+		reset: resetFirstNameInput,
+	} = useInput((v) => v.trim() !== "");
+
+	const {
+		value: enterLastName,
+		isValid: enteredLastNameIsValid,
+		hasError: lastNameHasError,
+		valueChangeHandler: lastNameChangeHandler,
+		inputBlurHandler: lastNameBlurHandler,
+		reset: resetLastNameInput,
+	} = useInput((v) => v.trim() !== "");
+
+	const {
+		value: enterEmail,
+		isValid: enteredEmailIsValid,
+		hasError: emailHasError,
+		valueChangeHandler: emailChangeHandler,
+		inputBlurHandler: emailBlurHandler,
+		reset: resetEmailInput,
+	} = useInput((v) => v.includes("@"));
+
+	// LOGIC
+	let formIsValid = false;
+
+	if (
+		enteredFirstNameIsValid &&
+		enteredLastNameIsValid &&
+		enteredEmailIsValid
+	) {
+		formIsValid = true;
+	}
+
+	const formSubmissionHandler = (e) => {
+		e.preventDefault();
+
+		if (
+			!enteredFirstNameIsValid ||
+			!enteredLastNameIsValid ||
+			!enteredEmailIsValid
+		) {
+			return;
+		}
+	};
+
+	// CSS LOGIC
+	const firstNameInputClasses = firstNameHasError
+		? "form-control invalid"
+		: "form-control";
+
+	const lastNameInputClasses = lastNameHasError
+		? "form-control invalid"
+		: "form-control";
+
+	const emailInputClasses = emailHasError
+		? "form-control invalid"
+		: "form-control";
+
+	return (
+		<form>
+			<div className="control-group">
+				<div className={firstNameInputClasses}>
+					<label htmlFor="name">First Name</label>
+					<input
+						type="text"
+						id="name"
+						onChange={firstNameChangeHandler}
+						onBlur={firstNameBlurHandler}
+						value={enterFirstName}
+					/>
+				</div>
+				<div className={lastNameInputClasses}>
+					<label htmlFor="name">Last Name</label>
+					<input
+						type="text"
+						id="name"
+						onChange={lastNameChangeHandler}
+						onBlur={lastNameBlurHandler}
+						value={enterLastName}
+					/>
+				</div>
+			</div>
+			<div className={emailInputClasses}>
+				<label htmlFor="name">E-Mail Address</label>
+				<input
+					type="email"
+					id="name"
+					onChange={emailChangeHandler}
+					onBlur={emailBlurHandler}
+					value={enterEmail}
+				/>
+			</div>
+			<div className="form-actions">
+				<button disabled={!formIsValid}>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default BasicForm;
