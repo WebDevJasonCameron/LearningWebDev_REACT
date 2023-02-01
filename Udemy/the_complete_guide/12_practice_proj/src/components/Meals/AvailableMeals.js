f582138f2c0f464d5397959714f9bd1af6498022
Jason Cameron <dev.jasoncameron@gmail.com>
2023-01-31 19:06:17 -0600

    data from firebase actually loads into app

--- Udemy/the_complete_guide/12_practice_proj/src/components/Meals/AvailableMeals.js
+++ Udemy/the_complete_guide/12_practice_proj/src/components/Meals/AvailableMeals.js
@@ -1,21 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 
 const AvailableMeals = () => {
+	const [meals, setMeals] = useState([]);
+
 	useEffect(() => {
 		const fetchMeals = async () => {
-			await fetch(
+			const response = await fetch(
 				"https://custom-hooks-react-tut-default-rtdb.firebaseio.com/meals.json"
 			);
+			const responseData = await response.json();
+
+			const loadedMeals = [];
+
+			for (const key in responseData) {
+				loadedMeals.push({
+					id: key,
+					name: responseData[key].name,
+					description: responseData[key].description,
+					price: responseData[key].price,
+				});
+			}
+			setMeals(loadedMeals);
 		};
 
 		fetchMeals();
 	}, []);
 
-	const mealsList = DUMMY_MEALS.map((meal) => (
+	const mealsList = meals.map((meal) => (
 		<MealItem
 			key={meal.id}
 			id={meal.id}
