45843e9d61fcc44d6eaa9084c9b3d988fcb9f58f
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-25 19:51:49 -0600

    passing obj for obj for onclick

--- Udemy/ztm_react_course/monster-rolodex/src/App.js
+++ Udemy/ztm_react_course/monster-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
 		super();
 
 		this.state = {
-			name: "Jason",
+			name: { firstName: "Jason", lastName: "Cameron" },
+			company: "Smash Co.",
 		};
 	}
 	render() {
@@ -16,10 +17,15 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
-					<p>Hi {this.state.name}</p>
+					<p>
+						Hi {this.state.name.firstName} {this.state.name.lastName}, I work at{" "}
+						{this.state.company}
+					</p>
 					<button
 						onClick={() => {
-							this.setState({ name: "Smash" });
+							this.setState({
+								name: { firstName: "Smash", lastName: "Monster" },
+							});
 							console.log(this.state);
 						}}
 					>
