e769183f27c4cab8f6846732389253860631e68f
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-27 16:03:47 -0600

    optimize: move func out, and destruction the this out

--- Udemy/ztm_react_course/monster-rolodex/src/App.js
+++ Udemy/ztm_react_course/monster-rolodex/src/App.js
@@ -29,11 +29,21 @@ class App extends Component {
 			);
 	}
 
+	onSearchChange = (e) => {
+		const searchField = e.target.value.toLocaleLowerCase();
+		this.setState(() => {
+			return { searchField };
+		});
+	};
+
 	render() {
 		console.log("2. Render");
 
-		const filteredMonsters = this.state.monsters.filter((monster) => {
-			return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+		const { monsters, searchField } = this.state;
+		const { onSearchChange } = this;
+
+		const filteredMonsters = monsters.filter((monster) => {
+			return monster.name.toLocaleLowerCase().includes(searchField);
 		});
 
 		return (
@@ -42,13 +52,7 @@ class App extends Component {
 					className="search-box"
 					type="search"
 					placeholder="Search Monsters"
-					onChange={(e) => {
-						console.log(e.target.value);
-						const searchField = e.target.value.toLocaleLowerCase();
-						this.setState(() => {
-							return { searchField };
-						});
-					}}
+					onChange={onSearchChange}
 				/>
 				{filteredMonsters.map((monster) => {
 					return (
