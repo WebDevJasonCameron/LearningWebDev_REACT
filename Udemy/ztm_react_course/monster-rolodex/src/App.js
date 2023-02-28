027e0abcce93437e3bfdb263a083cd57dd2f6129
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-28 17:15:06 -0600

    app is functional

--- Udemy/ztm_react_course/monster-rolodex/src/App.js
+++ Udemy/ztm_react_course/monster-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 
 import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
 
 import "./App.css";
 
@@ -41,11 +42,10 @@ class App extends Component {
 
 		return (
 			<div className="App">
-				<input
+				<SearchBox
 					className="search-box"
-					type="search"
-					placeholder="Search Monsters"
-					onChange={onSearchChange}
+					onChangeHandler={onSearchChange}
+					placeholder="search monsters"
 				/>
 				<CardList monsters={filteredMonsters} />
 			</div>
