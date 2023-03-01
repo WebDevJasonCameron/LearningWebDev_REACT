eb390b387a3c2129236aa749ff8d265bf6b2d865
Jason Cameron <dev.jasoncameron@gmail.com>
2023-02-28 20:10:53 -0600

    add cool card with monsters

--- Udemy/ztm_react_course/monster-rolodex/src/components/card-list/card-list.component.jsx
+++ Udemy/ztm_react_course/monster-rolodex/src/components/card-list/card-list.component.jsx
@@ -8,17 +8,21 @@ class CardList extends Component {
     const { monsters } = this.props;
 
     return (
-      <div>
-       {monsters.map(monster => (
+      <div className="card-list">
+       {monsters.map(monster => {
+        const { name, email, id } = monster;
+
+        return (
         <div className="card-container">
           <img 
-            src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} 
-            alt={`monster ${monster.name}`} 
+            src={`https://robohash.org/${id}?set=set2&size=180x180`} 
+            alt={`monster ${name}`} 
           />
-          <h2>{monster.name}</h2>
-          <p>{monster.email}</p>
+          <h2>{name}</h2>
+          <p>{email}</p>
         </div>
-       ))}
+
+       )})}
       </div>
     )
   }
