a2a278aaa3db32c6cd09fb0028c2ab3abf02baa6
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-03 20:44:55 -0600

    understanding useState causing useState infin render loop

--- Udemy/ztm_react_course/monster-rolodex/src/App.js
+++ Udemy/ztm_react_course/monster-rolodex/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
@@ -6,16 +6,23 @@ import SearchBox from "./components/search-box/search-box.component";
 import "./App.css";
 
 const App = () => {
+	const [searchField, setSearchField] = useState(""); // [value, setValue]
+	const [monsters, setMonsters] = useState([]);
 
-	const [] = useState();			// Gives back an array of two values
+	console.log("render");
 
 	const onSearchChange = (e) => {
-			const searchField = e.target.value.toLocaleLowerCase();
-			this.setState(() => {
-				return { searchField };
-			});
-		};
+		const searchFieldString = e.target.value.toLocaleLowerCase();
+		setSearchField(searchFieldString);
 	};
+
+	const filteredMonsters = monsters.filter((m) => {
+		return m.name.toLocaleLowerCase().includes(searchField);
+	});
+
+	fetch("https://jsonplaceholder.typicode.com/users")
+		.then((r) => r.json())
+		.then((u) => setMonsters(u));
 
 	return (
 		<div className="App">
@@ -29,51 +36,5 @@ const App = () => {
 		</div>
 	);
 };
-
-// ---------------------------------------------------------------
-
-// class App extends Component {
-// 	constructor() {
-// 		super();
-
-// 		this.state = {
-// 			monsters: [],
-// 			searchField: "",
-// 		};
-// 	}
-
-// 	componentDidMount() {
-// 		fetch("https://jsonplaceholder.typicode.com/users")
-// 			.then((r) => r.json())
-// 			.then((users) =>
-// 				this.setState(() => {
-// 					return { monsters: users };
-// 				})
-// 			);
-// 	}
-
-// 	onSearchChange
-
-// 	render() {
-// 		const { monsters, searchField } = this.state;
-// 		const { onSearchChange } = this;
-
-// 		const filteredMonsters = monsters.filter((monster) => {
-// 			return monster.name.toLocaleLowerCase().includes(searchField);
-// 		});
-
-// 		return (
-// 			<div className="App">
-// 				<h1 className="app-title"> Monsters Rolodex</h1>
-// 				<SearchBox
-// 					className="monsters-search-box"
-// 					onChangeHandler={onSearchChange}
-// 					placeholder="search monsters"
-// 				/>
-// 				<CardList monsters={filteredMonsters} />
-// 			</div>
-// 		);
-// 	}
-// }
 
 export default App;
