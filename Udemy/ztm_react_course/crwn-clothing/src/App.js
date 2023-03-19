e18e5695237acc92b12657c36a334df54ec3e1ac
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-19 09:22:49 -0500

    building a nav bar with Frag and Link

--- Udemy/ztm_react_course/crwn-clothing/src/App.js
+++ Udemy/ztm_react_course/crwn-clothing/src/App.js
@@ -1,7 +1,24 @@
+// Libs
+import { Routes, Route } from "react-router-dom";
+
+// Comp
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 
+// App
+const Shop = () => {
+	return <h1>I amd the Shop page</h1>;
+};
+
 const App = () => {
-	return <Home />;
+	return (
+		<Routes>
+			<Route path="/" element={<Navigation />}>
+				<Route index element={<Home />} />
+				<Route path="/shop" element={<Shop />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
