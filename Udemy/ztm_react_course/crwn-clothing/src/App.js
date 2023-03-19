0c0b1af6291c6ca34eca7ed80da980ba818e1f34
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-18 21:58:20 -0500

    look more into destruct of an obj

--- Udemy/ztm_react_course/crwn-clothing/src/App.js
+++ Udemy/ztm_react_course/crwn-clothing/src/App.js
@@ -1,5 +1,24 @@
 const App = () => {
-	return <div>Hello World</div>;
+	const categories = [
+		{
+			id: 1,
+			title: "Hats",
+		},
+	];
+
+	return (
+		<div className="categories-container">
+			{categories.map(({ title }) => (
+				<div className="category-container">
+					{/* {img /} */}
+					<div className="category-body-container">
+						<h2>{title}</h2>
+						<p>Shop Now</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
