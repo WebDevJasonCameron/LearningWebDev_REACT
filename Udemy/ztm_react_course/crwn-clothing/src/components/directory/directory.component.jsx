146a6964833a4c5182eb57e6c64694612fd16987
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-18 23:28:28 -0500

    styles and moveing compos into their own dir

---
+++ Udemy/ztm_react_course/crwn-clothing/src/components/directory/directory.component.jsx
@@ -0,0 +1,13 @@
+import CategoryItem from "../category-item/category-item.component"
+
+const Directory = ({categories}) => {
+  return(
+		<div className="directory-container">
+			{categories.map((category) => (
+				<CategoryItem key={categories.id} category={category} />
+			))}
+		</div>
+  )
+}
+
+export default Directory;
