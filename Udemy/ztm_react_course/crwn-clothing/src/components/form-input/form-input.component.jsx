0e6ea216f7f4ea16aa775c0539d76e8673c77dc0
Jason Cameron <dev.jasoncameron@gmail.com>
2023-03-24 03:56:21 -0500

    compo the form

---
+++ Udemy/ztm_react_course/crwn-clothing/src/components/form-input/form-input.component.jsx
@@ -0,0 +1,14 @@
+
+
+
+const FormInput = ({ label, ...otherProps }) =>{
+
+  return(
+    <div>
+      <label>{label}</label>
+      <input {...otherProps}/>
+    </div>
+  )
+}
+
+export default FormInput
