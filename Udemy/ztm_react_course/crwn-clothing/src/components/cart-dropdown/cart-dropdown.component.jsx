f3bc67064ff78420dc74f6b26987b05084fbea89
Jason Cameron <dev.jasoncameron@gmail.com>
2023-05-29 18:29:41 -0500

    added function that adds items and increases quantity of item if already selected

--- Udemy/ztm_react_course/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ Udemy/ztm_react_course/crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,15 @@
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   return (
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
-        <Button>Go To Checkout</Button>
+      <div className='cart-items' >
+        {[].map(item => <CartItem cartItem={item} />)}
+      </div>
+      <Button>Go To Checkout</Button>
     </div>
   )
 }
