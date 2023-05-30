bf68be36c10613a67ba0628f45b8a2b5b18cf8d2
Jason Cameron <dev.jasoncameron@gmail.com>
2023-05-29 19:13:08 -0500

    adding items to drop down works now

--- Udemy/ztm_react_course/crwn-clothing/src/contexts/cart.context.jsx
+++ Udemy/ztm_react_course/crwn-clothing/src/contexts/cart.context.jsx
@@ -1,29 +1,22 @@
 import { createContext, useState } from 'react';
 
 const addCartItem = (cartItems, productToAdd) => {
-  // find if cartItems contains productToAdd
   const exitingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
 
-  // if found, increment quantity
   if(exitingCartItem){
     return cartItems.map((cartItem) => cartItem.id === productToAdd.id ?
     {...cartItem, quantity: cartItem.quantity + 1}
     : cartItem)
   }
 
-  // return new array with modified cartItems with new cart item
   return [...cartItems, {...productToAdd, quantity: 1}];
 }
-
 
 export const CartContext = createContext({
   isCartOpen: false,
   setIsOpen: () => {},
   cartItems: [],
   addItemToCart: () => {}
-
-
-
 });
 
 export const CartProvider = ({ children }) => {
@@ -34,7 +27,7 @@ export const CartProvider = ({ children }) => {
     setCartItems(addCartItem(cartItems, productToAdd))
   }
 
-  const value = { isCartOpen, setIsCartOpen };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems};
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
