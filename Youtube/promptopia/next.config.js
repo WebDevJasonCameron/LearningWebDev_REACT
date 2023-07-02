6307602613d10e79a24cf957e4106a52e219b861
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-02 11:06:22 -0500

    working on break in menu

--- Youtube/promptopia/next.config.js
+++ Youtube/promptopia/next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
+
+// OVERRIDE THE FOLLOWING CODE
+/*
 const nextConfig = {}
+
+module.exports = nextConfig
+*/
+
+// FROM WRITER'S GH
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+    },
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
