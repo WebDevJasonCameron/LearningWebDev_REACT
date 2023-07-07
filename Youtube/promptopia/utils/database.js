6c71df6b331f4a18955e93dbb936623deaf3f113
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-06 20:10:28 -0500

    fixed db connection

--- Youtube/promptopia/utils/database.js
+++ Youtube/promptopia/utils/database.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
-let isConnected = false;    // track the connection status
+let isConnected = false; // track the connection
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
 
     if(isConnected) {
-        console.log('MongoDB is already connected')
+        console.log('MongoDB is already connected');
         return;
     }
 
@@ -16,7 +16,10 @@ export const connectToDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
+
         isConnected = true;
+
+        console.log('MongoDB connected')
     } catch (error) {
         console.log(error);
     }
