431db562fa4431d8184b7f94825a86b011e93dc2
Jason Cameron <dev.jasoncameron@gmail.com>
2023-07-09 08:18:55 -0500

    working on showing feeds

--- Youtube/promptopia/components/Feed.jsx
+++ Youtube/promptopia/components/Feed.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+'use client';
+
+import { useState, useEffect } from "react";
+
+import PromptCard from "@components/PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className="mt-16 prompt_layout">
+
+        </div>
+    )
+}
 
 const Feed = () => {
+    const [searchText, setSearchText] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    const handleSearchChange = (e) => {}
+
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await fetch('/api/prompt');
+            const data = await response.json();
+
+            setPosts(data);
+        }
+
+        fetchPost();
+    }, [])
+
+
     return (
-        <div>
-            Feed
-        </div>
+        <section className="feed">
+            <form className="relative w-full flex-center">
+                <input
+                    type="text"
+                    placeholder="Search for a tag or a username"
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                    className="search_input peer"
+                />
+            </form>
+
+            <PromptCardList
+                data={[]}
+                handleTagClick={() => {}}
+            />
+        </section>
     );
 };
 
